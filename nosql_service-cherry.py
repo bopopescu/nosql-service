c8c215d8a595ee544ef3601d2cdad0f6ded9d262
pgregg <pgregg@playfirst.com>
2013-10-31 13:02:11 -0700

    cherrypy added and http://localhost:8080/pf_create_table?name=mortalinstruments API started

---
+++ nosql_service-cherry.py
@@ -0,0 +1,41 @@
+import simplejson as json
+import cherrypy
+from cherrypy import expose
+
+import time
+
+import sys
+sys.path.append('/Users/pgregg/nosql-service')
+sys.path.append('/Users/pgregg/nosql-service/tests')
+sys.path.append('/Users/pgregg/nosql-service/tests/integration')
+sys.path.append('/Users/pgregg/nosql-service/tests/integration/dynamodb2')
+
+import unittest
+from boto.dynamodb2 import exceptions
+from boto.dynamodb2.fields import HashKey, RangeKey, KeysOnlyIndex
+from boto.dynamodb2.items import Item
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.types import NUMBER
+    
+dynamodb = True
+
+class nosql_service:
+
+    @expose
+    def pf_create_table(self, name):
+        # creating a full table with all options specified.
+        users = Table.create(name, schema=[
+            HashKey('username'),
+            RangeKey('friend_count', data_type=NUMBER)
+        ], throughput={
+            'read': 5,
+            'write': 5,
+        }, indexes=[
+            KeysOnlyIndex('LastNameIndex', parts=[
+                HashKey('username'),
+                RangeKey('last_name')
+            ]),
+        ])
+        return json.dumps(name + " table created")
+
+cherrypy.quickstart(nosql_service())
